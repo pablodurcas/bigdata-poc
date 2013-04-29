3698c38c78a4559fd5cff65c155b9eed90796f82
Javi Roman <sarnoso@ocelote.(none)>
2013-04-29 09:12:07 +0200

    api java test

--- entry-level-tests/cpp/run.sh
+++ entry-level-tests/cpp/run.sh
@@ -8,9 +8,3 @@ sudo hadoop pipes \
 #-D hadoop.pipes.java.recordreader=true \
 #-D hadoop.pipes.java.recordwriter=true \
 
-# sudo hadoop namenode -format
-# sudo hadoop fs -put ../rawdata/ rawdata
-# sudo hadoop fs -put max_temperature bin/max_temperature
-#sudo hadoop fs -rm /user/root/bin/max_temperature
-#hadoop dfsadmin -safemode leave
-# sudo hadoop fs -ls
