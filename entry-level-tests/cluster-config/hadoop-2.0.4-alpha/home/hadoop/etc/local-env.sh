88185f6520e8b18dc8420bc773e748af6a93bb0c
hadoop <hadoop@hdfs01.hpc.cediant.es>
2013-05-06 16:17:59 +0200

    minimal server configurations

---
+++ entry-level-tests/cluster-config/hadoop-2.0.4-alpha/home/hadoop/etc/local-env.sh
@@ -0,0 +1,9 @@
+# User specific aliases and functions
+export JAVA_HOME=/usr/java/jdk1.7.0_21/
+export HADOOP_CONF_DIR=/home/hadoop/etc
+export HADOOP_HOME=/opt/hadoop
+export HADOOP_LOG_DIR=/home/hadoop/log
+export HADOOP_PID_DIR=/home/hadoop/run
+export PATH=$PATH:/opt/hadoop/bin/:/opt/hadoop/sbin/:/usr/java/jdk1.7.0_21/bin/
+export HADOOP_YARN_USER=hadoop
+export HADOOP_YARN_HOME=/opt/hadoop
